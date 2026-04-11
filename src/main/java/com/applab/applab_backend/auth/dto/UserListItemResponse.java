27c871dfdeec2a49d577808bdcb33a8afaf5b330
Arnab Khan <aranabkhan@gmail.com>
2026-04-11 18:02:36 +0530

    fixes

--- src/main/java/com/applab/applab_backend/auth/dto/UserListItemResponse.java
+++ src/main/java/com/applab/applab_backend/auth/dto/UserListItemResponse.java
@@ -4,13 +4,16 @@ import java.time.Instant;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @Data
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserListItemResponse {
     private Long id;
     private String name;
     private String username;
+    private String bio;
     private Instant createdAt;
     private Instant updatedAt;
     private String profileImageUrl;
